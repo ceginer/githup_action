0c39c3d71a9b355300890c2c0f4e1b2b0cb4c24a
Son <rooneei@naver.com>
2023-07-07 08:37:27 +0900

    [feat]home

---
+++ src/main/java/com/crudPost/Post/Controller/HomeController.java
@@ -0,0 +1,29 @@
+//package com.crudPost.Post.Controller;
+//
+//import com.crudPost.Post.Dto.SignupUserDto;
+//import com.crudPost.Post.Service.BoardService;
+//import com.crudPost.Post.Service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Slf4j
+//@Controller
+//@RequiredArgsConstructor
+//public class HomeController {
+////
+//    private final BoardService boardService;
+//    private final UserService userService;
+//
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+//}
